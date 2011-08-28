d76b83451582445aad9c2fb8ce8633b0e3cae125
Stephen Clavering <stephen@clavering.me.uk>
2011-08-28 19:31:06 +0100

    VolumeView.m: fix formatting

--- src/widgets/VolumeView.m
+++ src/widgets/VolumeView.m
@@ -40,15 +40,16 @@
 
 @implementation VolumeView
 
--(id)initWithCoder:(id)aCoder{
-    self =[super initWithCoder:aCoder];
-    volume=1.0;
+-(id)initWithCoder:(id)aCoder
+{
+    self = [super initWithCoder:aCoder];
+    volume = 1.0;
     return self;
 }
 
 - (BOOL)acceptsFirstResponder
 {
-	return NO;
+    return NO;
 }
 
 -(void)setVolume:(float)aVolume{
@@ -58,23 +59,15 @@
 }
 
 - (void)drawRect:(NSRect)aRect{
-    [super drawRect:aRect];   
-        float hue;
-        float top;
-        if (volume <=1.0){
-            hue =117.0/360.0;
-            top =1.0;
-        }else{
-            hue = 0.0;
-            top =2.0;
-        }
-            
-        [[NSColor colorWithDeviceHue:hue
-                         saturation:.5
-                         brightness:(top/volume)
-                              alpha:1.0] set];
-        
-        NSRectFill(NSMakeRect(20,15,32*volume,8));
+    [super drawRect:aRect];
+    float hue = 0.0;
+    float top = 2.0;
+    if(volume <= 1.0) {
+        hue = 117.0 / 360.0;
+        top = 1.0;
+    }
+    [[NSColor colorWithDeviceHue:hue saturation:0.5 brightness:top/volume alpha:1.0] set];
+    NSRectFill(NSMakeRect(20, 15, 32 * volume, 8));
 }
 
 @end
