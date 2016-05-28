e7caba3ead831faa478699d25c43cb08acbb0125
Stephen Clavering <stephen@clavering.me.uk>
2016-05-28 16:55:30 +0100

    NoirScrubber.m: fix indentation

--- src/NoirScrubber.m
+++ src/NoirScrubber.m
@@ -24,31 +24,31 @@
 
 - (void)drawRect:(NSRect)rect
 {
-        [self lockFocus];
+    [self lockFocus];
 
-        CGPoint points[2];
-        points[0] = CGPointMake(OFFSET, floor(self.frame.size.height / 2.0));
-        points[1] = CGPointMake(self.frame.size.width - OFFSET, floor(self.frame.size.height / 2.0));
+    CGPoint points[2];
+    points[0] = CGPointMake(OFFSET, floor(self.frame.size.height / 2.0));
+    points[1] = CGPointMake(self.frame.size.width - OFFSET, floor(self.frame.size.height / 2.0));
 
-        CGContextRef cgRef = [NSGraphicsContext currentContext].graphicsPort;
-        CGContextSetAllowsAntialiasing(cgRef, NO);
-        CGContextSetGrayStrokeColor(cgRef, 1.0, 1.0);
-        CGContextSetLineWidth(cgRef, 1);
-        CGContextSetLineCap(cgRef, kCGLineCapRound);
-        CGContextStrokeLineSegments(cgRef, points, 2);
-        CGContextSetAllowsAntialiasing(cgRef, YES);
+    CGContextRef cgRef = [NSGraphicsContext currentContext].graphicsPort;
+    CGContextSetAllowsAntialiasing(cgRef, NO);
+    CGContextSetGrayStrokeColor(cgRef, 1.0, 1.0);
+    CGContextSetLineWidth(cgRef, 1);
+    CGContextSetLineCap(cgRef, kCGLineCapRound);
+    CGContextStrokeLineSegments(cgRef, points, 2);
+    CGContextSetAllowsAntialiasing(cgRef, YES);
 
-        float scrubWidth = self.frame.size.height;
-        float scrubHeight = self.frame.size.height;
+    float scrubWidth = self.frame.size.height;
+    float scrubHeight = self.frame.size.height;
 
-        [[NSGraphicsContext currentContext] saveGraphicsState];
-        [NSGraphicsContext currentContext].imageInterpolation = NSImageInterpolationHigh;
-        NSPoint tPoint = NSMakePoint(OFFSET + ([self doubleValue] * (self.frame.size.width - (OFFSET * 2))), self.frame.size.height / 2);
-        NSRect tScrubRect = NSMakeRect(tPoint.x, tPoint.y, scrubWidth, scrubHeight);
-        [scrub drawInRect:NSIntegralRect(NSOffsetRect(tScrubRect, -tScrubRect.size.width / 2.0, -tScrubRect.size.height / 2.0)) fromRect:NSMakeRect(0, 0, scrub.size.width, scrub.size.height) operation:NSCompositeSourceOver fraction:1.0];
-        [[NSGraphicsContext currentContext] restoreGraphicsState];
+    [[NSGraphicsContext currentContext] saveGraphicsState];
+    [NSGraphicsContext currentContext].imageInterpolation = NSImageInterpolationHigh;
+    NSPoint tPoint = NSMakePoint(OFFSET + ([self doubleValue] * (self.frame.size.width - (OFFSET * 2))), self.frame.size.height / 2);
+    NSRect tScrubRect = NSMakeRect(tPoint.x, tPoint.y, scrubWidth, scrubHeight);
+    [scrub drawInRect:NSIntegralRect(NSOffsetRect(tScrubRect, -tScrubRect.size.width / 2.0, -tScrubRect.size.height / 2.0)) fromRect:NSMakeRect(0, 0, scrub.size.width, scrub.size.height) operation:NSCompositeSourceOver fraction:1.0];
+    [[NSGraphicsContext currentContext] restoreGraphicsState];
 
-        [self unlockFocus];
+    [self unlockFocus];
 }
 
 -(void)setDoubleValue:(double)aValue
